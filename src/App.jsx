7f330d64d6884bc439e7f9253bd3b1e2c36eb25f
bjkim <bjkimgithub@gmail.com>
2025-11-08 00:16:13 +0900

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// 홈페이지  /
+// 영화 전체 보여주는 페이지 (서치)  /movies
+// 영화 디테일 페이지  /movies/:id
+// 추천 영화
+// 리뷰
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/movies">
+            <Route index element={<MoviePage />} />
+            <Route path=":id" element={<MovieDetailPage />} />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;

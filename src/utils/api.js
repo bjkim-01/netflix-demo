f21ef2bf0cf1f3bbbb9f2a2b801996164ea37cca
bjkim <bjkimgithub@gmail.com>
2025-11-08 00:40:39 +0900

    forth commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Accept:'',
+    Accept:'application/json',
     Authorization:`Bearer ${API_KEY}`,
   },
 });
